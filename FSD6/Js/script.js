565ef53ea3b9e901454d7caee736dc9aaf29cbb7
pankajoberoi <oberoipankaj449@gmil.com>
2024-01-25 00:04:14 +0530

    updated

--- FSD6/Js/script.js
+++ FSD6/Js/script.js
@@ -1,9 +1,154 @@
 console.log("Welcome to fsd6");
 
-console.log("Hello world");
+// console.log("Hello world");
 
-let firstName="Pankaj";
+// let firstName="Pankaj";
 
-if(firstName==="Pankaj"){
-    alert("Welcome");
-}
+// if(firstName==="Pankaj"){
+//     alert("Welcome");
+// }
+
+
+// let name1="Pankaj";
+// console.log(name1);
+
+// let age=21;
+// console.log(age);
+
+// let $firstName="Shivani";
+
+// let last_Name="Arya";
+
+// console.log($firstName);
+// console.log(last_Name);
+
+// let PI=3.14
+
+// let job1="developer"
+// let job2="teacher"
+
+// let profile_of_first_job="developer";
+// let profile_of_second_job="teacher";
+
+
+// Data types
+// number -> decimal int
+//string
+//bool
+
+// let college='o';
+
+// let college2="oriental";
+
+//undefined
+//null
+
+// let age;
+// console.log(age);
+
+// let subject= "English";
+
+// let subject1=1;
+
+// console.log(subject);
+
+// let i_love_Js=false;
+
+// let javaScriptIsFun;
+// console.log(i_love_Js)
+
+// console.log(typeof true);
+// console.log(typeof i_love_Js);
+// console.log(typeof 23.34)
+// console.log(typeof "udit");
+// console.log(typeof 'T');
+// javaScriptIsFun='Yes!'
+// console.log( typeof javaScriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof undefined);
+
+// year="10";
+// console.log(year);
+
+// var let const
+
+
+// const birthYear=2000;
+// let currentYear=2024;
+// let age=currentYear-birthYear;
+
+// console.log(age);
+
+// var batch="fsd6";
+// console.log(batch);
+
+// var batch="fsd10";
+// console.log(batch);
+
+// basic operator
+
+
+
+// let age=2024-2000;
+// console.log(age);
+
+// let experience=2;
+//  age=24+experience;
+//  console.log(age);
+
+// console.log(age*2,age/2);
+// console.log(age);
+
+// console.log(10 ** 3);
+
+
+// const firstName="Pankaj";
+// const lastName="Oberoi";
+
+// console.log(firstName+' '+ lastName);
+
+// const exp=2;
+// const age=18;
+
+// console.log(exp+age);
+
+// let score=50;
+// +=,-=,*=,/=
+
+// 10+=age;
+
+// console.log(score+=10);
+// console.log(score*=10);
+// console.log(score/=10);
+// console.log(score-=10);
+
+// let age=50;
+// age++;
+// console.log(age);
+
+// console.log(age++);
+
+// let score=20;
+// //increment -> use
+// console.log(++score);
+
+// let score1=40;
+// //use -> incremnet
+// console.log(score1++);
+// console.log(score1);
+
+
+// const ajay=20;
+// const anu=30;
+// console.log(ajay<anu);
+
+// task comparison ke sath how maths work
+
+
+let currentYear=2024;
+
+console.log(currentYear+=10 < (currentYear+=25));
+
+
