0a4a8def10b85d8372527ee63d13d99793fca8cb
pankajoberoi <poberoi1557.ca19@chitkara.edu.in>
2024-02-16 09:53:43 +0530

    updated

--- FSD6/Js/fundamentals_part2.js
+++ FSD6/Js/fundamentals_part2.js
@@ -16,11 +16,11 @@
 // }
 // logger();
 
-function fruitProcessor(apples,oranges){
-    console.log(apples+" apples " + oranges + " oranges")
-    const juice=`Juice with ${apples} apples and ${oranges} oranges`;
-    return juice;
-}
+// function fruitProcessor(apples,oranges){
+//     console.log(apples+" apples " + oranges + " oranges")
+//     const juice=`Juice with ${apples} apples and ${oranges} oranges`;
+//     return juice;
+// }
 
 
 
@@ -40,25 +40,60 @@ function fruitProcessor(apples,oranges){
 
 // function declaration vs expression
 
-const age1=calAge1(2001);
-console.log(age1);
-function calAge1(birthYear){
-    return 2024-birthYear;
-}
+// const age1=calAge1(2001);
+// console.log(age1);
+// function calAge1(birthYear){
+//     return 2024-birthYear;
+// }
 
 
 // const age=calAge1(2000)
 // console.log(age);
 
-const age2=calAge2(2000);
-console.log(age2); 
-const calAge2=function(birthYear){
-    return 2024-birthYear;
-}
+// const age2=calAge2(2000);
+// console.log(age2); 
 
-// const age2=calAge2(2001);
+
+
+
+// const calAge2=function(birthYear=2001){
+//     return 2024-birthYear;
+// }
+
+
+// const age2=calAge2();
 // console.log(age2);
 
+
+
+// const calAge3= birthYear => 2024-birthYear;
+
+
+
+// console.log(calAge3(2000));
+// console.log(birthYear);
+
+
+// const retirement = (birthyear,firstName) =>{
+//     const age=2024-birthyear;
+//     const retirement=62-age;
+//     return `${firstName} retires in ${retirement} years`;
+// }
+
+// alert(retirement(2000,"Aditi"));
+
+// const cutPieces=function(fruit){
+//     return fruit*4;
+// }
+
+// function fruitProcessor(apples,oranges){
+//     const applePieces=cutPieces(apples);
+//     const orangePieces=cutPieces(oranges);
+//     const juice=`Juice with ${applePieces} pieces of apples and ${orangePieces} pieces of oranges`;
+//     return juice;
+// }
+// let glass=fruitProcessor(2,4);
+// console.log(glass);
 
 
 
