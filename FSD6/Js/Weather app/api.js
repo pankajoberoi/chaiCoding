f15f53fcbc7a8096540af684029e4cef46f3a537
pankajoberoi <poberoi1557.ca19@chitkara.edu.in>
2024-03-27 18:49:01 +0530

    weather

--- FSD6/Js/Weather app/api.js
+++ FSD6/Js/Weather app/api.js
@@ -1,15 +1,14 @@
 const URL = "https://cat-fact.herokuapp.com/facts";
 const factPara = document.querySelector("#fact");
-const btn = document.querySelector("#btn");
+const button = document.querySelector("#btn");
 
-const getFacts = async () => {
-  console.log("getting data....");
+async function getFacts() {
+  console.log("getting data.......");
   let response = await fetch(URL);
-  console.log(response); //JSON format //XML
+  console.log(response);
   let data = await response.json();
-  console.log(data[0].text);
-  factPara.innerText=data[0].text
-};
+  console.log(data[1].text);
+  factPara.innerText = data[1].text;
+}
 
-btn.addEventListener("click", getFacts);
-
+button.addEventListener("click", getFacts);
