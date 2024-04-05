2d9842ddd768e1c607edbfdfeaed449db99d53f7
pankajoberoi <poberoi1557.ca19@chitkara.edu.in>
2024-04-05 20:17:50 +0530

    updated

--- FSD6/Js/QuizApp/script.js
+++ FSD6/Js/QuizApp/script.js
@@ -37,3 +37,75 @@ const questions=[
         ]
     }
 ];
+
+const questionElement=document.getElementById("question")
+const answerButton=document.getElementById("answer-buttons")
+const nextButton=document.getElementById("next-btn")
+
+
+let currentQuestionIndex=0;
+let score=0;
+
+function startQuiz(){
+    currentQuestionIndex=0;
+    score=0;
+    nextButton.innerHTML="Next";
+    showQuestion()
+}
+
+function showQuestion(){
+    resetstate();
+    let currentQuestion=questions[currentQuestionIndex]
+    let questionNo=currentQuestionIndex+1;
+
+    questionElement.innerHTML=questionNo + " . "  +  currentQuestion.question;
+
+    currentQuestion.answers.forEach((answer)=>{
+        const button=document.createElement("button");
+        button.innerHTML=answer.text;
+        button.classList.add("btn");
+        answerButton.appendChild(button);
+
+        if(answer.correct){
+            button.dataset.correct=answer.correct;
+        }
+
+        button.addEventListener("click",selectAnswer)
+
+
+    })
+
+
+}
+function resetstate(){
+    nextButton.style.display="none"
+    while(answerButton.firstChild){
+        answerButton.removeChild(answerButton.firstChild)
+    }
+}
+
+function selectAnswer(event){
+   
+    const selectedBtn=event.target;
+    const isCorrect=selectedBtn.dataset.correct==="true"
+
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    }
+    else{
+        selectedBtn.classList.add("incorrect");
+    }
+
+    Array.from(answerButton.children).forEach((button)=>{
+        if(button.dataset.correct==="true"){
+            selectedBtn.classList.add("correct");
+        }
+        button.disabled=true;
+    })
+    nextButton.style.display="block"
+
+}
+
+
+startQuiz();
