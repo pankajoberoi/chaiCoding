6d91dc5f00cce7212c7d9845e84350920b4b49a3
pankajoberoi <poberoi1557.ca19@chitkara.edu.in>
2024-04-05 18:00:25 +0530

    updated

---
+++ FSD6/Js/QuizApp/script.js
@@ -0,0 +1,39 @@
+const questions=[
+    {
+        question:"which one from these is language for front-end?",
+        answers:[
+            {text:"html" , correct:true},
+            {text:"node" , correct:false},
+            {text:"express" , correct:false},
+            {text:"c++" , correct:false},
+
+        ]
+    },
+    {
+        question: "which is smallest continent in world?",
+        answers: [
+            {text:"Asia" , correct:false},
+            {text:"Australia" , correct:true},
+            {text:"Arctic" , correct:false},
+            {text:"Africa" , correct:false},
+        ]
+    },
+    {
+        question: "which is language is server side?",
+        answers: [
+            {text:"Node" , correct:true},
+            {text:"html" , correct:false},
+            {text:"css" , correct:false},
+            {text:"Js" , correct:false},
+        ]
+    },
+    {
+        question: "which is largest desert in the world?",
+        answers: [
+            {text:"kalahari" , correct:false},
+            {text:"Gobi" , correct:false},
+            {text:"sahara" , correct:false},
+            {text:"Antarctica" , correct:true},
+        ]
+    }
+];
