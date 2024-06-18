06899e9ab9a9e3ee1bc31c41ffdb55ac9eadc1a9
pankajoberoi <poberoi1557.ca19@chitkara.edu.in>
2024-06-18 18:49:23 +0530

    updated

--- Ind_Fsd/react_begin/src/index.js
+++ Ind_Fsd/react_begin/src/index.js
@@ -1,26 +1,46 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 
+// let firstName = prompt("Enter Your name");
+// let age = prompt("Enter Yoru age");
 
-// let firstName=prompt("Enter Your name");
-// let age=prompt("Enter Yoru age");
+// const element = (
+//   <h1>
+//     My name is {firstName.toUpperCase()} and my age is {age}
+//   </h1>
+// );
 
-// const element=<h1>My name is {firstName} and my age is {age}</h1>
+// const element=<p>hello mitron !!!</p><p>Nice to meet you</p>
 
-// const element=<>
-//     <p>hello mitron !!!</p><p>Nice to meet you</p>
-//     </>
+// const element=(
+//     <div>
+//         <p>Hello world</p>
+//         <p>Mai kyon ayya yahan</p>
+//     </div>
+// )
 
+//coditional rendering 
+// let isLoggedIn=false;
 
+// const element=(
+//     <div>
+//         <h1>Hello {isLoggedIn?"Dhoni":"Kon hai be tu"}</h1>
+//     </div>
+// )
+
+const toggle="light";
 const element=(
     <div>
-        <p>Hello world</p>
-        <p>Mai kyon ayya yahan</p>
+        <h1 className={toggle}>Jai shree Ram</h1>
     </div>
 )
 
 
 
-let rootEl=document.getElementById("root")
 
-ReactDOM.render(element,rootEl)
+
+
+
+let rootEl = document.getElementById("root");
+
+ReactDOM.render(element, rootEl);
